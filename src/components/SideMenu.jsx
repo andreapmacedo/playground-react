a8c64a62a556031e30befd395e0ffa14f5cf09ea
Andre Macedo <aa.andremacedo@gmail.com>
2023-01-23 19:55:32 -0300

    feat: Implementa o SideMenu.

---
+++ src/components/SideMenu.jsx
@@ -0,0 +1,18 @@
+
+export default function SideMenu(props) {
+  return (
+    <>
+      <h1>SideMenu</h1>
+      {console.log(props.menuItem.length)}
+      {props.menuItem.map((item) => {
+        return (
+          <div key={item.title}>
+          <h3>{item.title}</h3>
+          </div>
+        )}
+      )}
+    </>
+  );
+}
+  
+  
